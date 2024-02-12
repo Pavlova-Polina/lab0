5062ed1bd6905c0bc3a4987f234c6ee47bc8a9e7
Pavlova-Polina <pavlova.polina@chnu.edu.ua>
2024-02-12 09:32:32 +0200

    replace symbol from << to "

--- lab0/lab0.cpp
+++ lab0/lab0.cpp
@@ -1,40 +1,53 @@
 ﻿// lab0.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-	const int MAX = 100;
-	double A[MAX], B[MAX], C[MAX];
-	int nA, nB, nC, i, m;
-	do {
-		cout << « Input number N < « << MAX << endl;
-		cin >> nA;
-	} while (nA<0 || nA>MAX);
-	nB = nC = 0;
-	for (i = 0; i < nA; i++)
-	{
-		cout << «A[« << i << «] = «; cin >> A[i];
-	}
-	do {
-		cout << « Input index m(0 <= m <= « << nA - 1 << «) « << endl;
-		cin >> m;
-	} while (m < 0 || m >= nA);
-	for (i = 0; i < nA; i++)
-		if (A[i] > A[m]) B[nB++] = A[i];
-		else C[nC++] = A[i];
-	cout << « Rezult « << endl;
-	cout << « B : « << endl;
-	for (i = 0; i < nB; i++)
-		cout << B[i] << «\t»;
-	cout << «C « << endl;
-	for (i = 0; i < nC; i++)
-		cout << C[i] << «\t»;
-	cin >> i;
-	cout << «Exit\n»;
-	return 0;
+
+int main() {
+    const int MAX = 100;
+    double A[MAX], B[MAX], C[MAX];
+    int nA, nB, nC, i, m;
+
+    do {
+        cout << "Input number N <" << MAX << endl;
+        cin >> nA;
+    } while (nA < 0 || nA > MAX);
+
+    nB = nC = 0;
+
+    for (i = 0; i < nA; i++) {
+        cout << "A[" << i << "]= ";
+        cin >> A[i];
+    }
+
+    do {
+        cout << "Input index m (0 <= m <= " << nA - 1 << ")" << endl;
+        cin >> m;
+    } while (m < 0 || m >= nA);
+
+    for (i = 0; i < nA; i++) {
+        if (A[i] > A[m])
+            B[nB++] = A[i];
+        else
+            C[nC++] = A[i];
+    }
+
+    cout << "Result:" << endl;
+    cout << "B :" << endl;
+    for (i = 0; i < nB; i++)
+        cout << B[i] << "\t";
+    cout << endl;
+
+    cout << "C :" << endl;
+    for (i = 0; i < nC; i++)
+        cout << C[i] << "\t";
+    cout << endl;
+
+    cout << "Exit" << endl;
+    return 0;
 }
+
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
